97a999f607854b6755db5efe4cc20ba8e0c3b636
helloyangdan <helloyangdan@gmail.com>
2015-05-03 16:40:14 +0800

    About Screen

---
+++ BullsEye/BullsEye/AboutViewController.m
@@ -0,0 +1,42 @@
+//
+//  AboutViewController.m
+//  BullsEye
+//
+//  Created by danyang on 15/5/3.
+//  Copyright (c) 2015年 dan yang. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)close
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
